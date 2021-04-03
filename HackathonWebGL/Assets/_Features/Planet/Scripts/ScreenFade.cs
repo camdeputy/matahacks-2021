ffa10fcf14816897712394a608e1483b50ece620
cody-rowland <68308819+cody-rowland@users.noreply.github.com>
2021-04-03 04:38:27 -0700

    Added Rocket Launch and Camera Zoom

---
+++ HackathonWebGL/Assets/_Features/Planet/Scripts/ScreenFade.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenFade : MonoBehaviour
+{
+    private Material currentMat;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //currentMat = renderer.material;
+        currentMat = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartFadeout()
+    {
+        Debug.Log("Starting Fade.");
+        StartCoroutine(FadeOut());
+    }
+
+    private IEnumerator FadeOut()
+    {
+        var alphaVal = currentMat.color.a;
+
+        while (alphaVal < 1)
+        {
+            Color color = currentMat.color;
+            float currentAlpha = color.a;
+            currentAlpha = Mathf.Min(currentAlpha + .005f, 1.0f);
+            color.a = currentAlpha;
+            currentMat.color = color;
+            alphaVal = currentAlpha;
+            yield return null;
+        }
+    }
+}
