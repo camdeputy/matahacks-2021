fd9f42677ed46cc069c83d3fc855a9bef30e7e9f
Cody <cody.rowland19@gmail.com>
2021-04-03 23:09:25 -0700

    Added Text

--- HackathonWebGL/Assets/_Features/Rocket/Scripts/JavascriptHook.cs
+++ HackathonWebGL/Assets/_Features/Rocket/Scripts/JavascriptHook.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class JavascriptHook : MonoBehaviour
 {
-
-    public Rocket rocket; 
+    public Rocket rocket;
+    public TextMeshProUGUI name;
+    public TextMeshProUGUI co2;
+    public TextMeshProUGUI kg_co2;
+    public TextMeshProUGUI passengers;
+    public TextMeshProUGUI tons_co2_passenger;
+    public TextMeshProUGUI fuelType;
 
     // Start is called before the first frame update
     void Start()
@@ -24,5 +30,9 @@ public class JavascriptHook : MonoBehaviour
 
     public void LaunchRocket(){
         rocket.LaunchRocket();
+    }
+
+    public void SelectRocket(string rocketName){
+        name.text = "Rocket Name: " + rocketName;
     }
 }
