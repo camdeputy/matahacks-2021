aef47d9d85396435fe8f829a5788ad8381a2002d
Kevin Beltran <kevinbeltranx@gmail.com>
2021-04-04 12:34:46 -0700

    [InitialCommit] Using Earth model from echoAR

---
+++ HackathonWebGL/Assets/_Features/EchoAR/Scripts/MaterialReplacer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialReplacer : MonoBehaviour
+{
+    [SerializeField] private Material replacementMaterial;
+
+    private bool _hasReplaced;
+
+    private void Update()
+    {
+        if (_hasReplaced) return;
+        
+        var meshRenderer = gameObject.GetComponentInChildren<MeshRenderer>();
+        if (!meshRenderer) return;
+        
+        meshRenderer.material = replacementMaterial;
+        _hasReplaced = true;
+    }
+}
