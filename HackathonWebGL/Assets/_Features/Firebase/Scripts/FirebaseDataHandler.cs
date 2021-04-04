6821cf4c6a302acc12dacbc48e64ec24f2b6aee0
Kevin Beltran <kevinbeltranx@gmail.com>
2021-04-04 09:41:47 -0700

    Fetching and populating UI from firebase data

---
+++ HackathonWebGL/Assets/_Features/Firebase/Scripts/FirebaseDataHandler.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using SimpleFirebaseUnity;
+using UnityEngine;
+
+
+public class FirebaseDataHandler : MonoBehaviour
+{
+    private Firebase _firebase;
+    private Dictionary<string, Dictionary<string, string>> _rocketData;
+    
+    private const string FIREBASE_DOMAIN = "https://rocket2trees-default-rtdb.firebaseio.com";
+    private const string ERROR = "ERROR";
+
+
+    private void Awake()
+    {
+        _firebase = Firebase.CreateNew(FIREBASE_DOMAIN);
+        _firebase.OnGetSuccess += InitializeData;
+        
+        GetDataFromFirebase();
+    }
+    
+    private void InitializeData(Firebase sender, DataSnapshot snapshot)
+    {
+        _rocketData = new Dictionary<string, Dictionary<string, string>>();
+        Dictionary<string, object> dict = snapshot.Value<Dictionary<string, object>>();
+        var rockets = snapshot.Keys;
+        if (rockets == null)
+        {
+            Debug.Log($"{nameof(rockets)} is null.");
+            return;
+        }
+            
+        foreach (var rocket in rockets)
+        {
+            _rocketData.Add(rocket, new Dictionary<string, string>());
+            if (!dict.ContainsKey(rocket))
+            {
+                Debug.Log($"{rocket} is not a valid key");
+                continue;
+            }
+
+            var rocketValues = (Dictionary<string, object>) dict[rocket];
+            if (rocketValues == null)
+            {
+                Debug.Log($"{nameof(rocketValues)} is null.");
+                continue;
+            }
+                
+            foreach (var value in rocketValues)
+            {
+                _rocketData[rocket].Add(value.Key, value.Value.ToString());
+            }
+        }
+    }
+
+    private void GetDataFromFirebase()
+    {
+        _firebase.GetValue(FirebaseParam.Empty.OrderByKey().LimitToFirst(2));
+    }
+
+    public string GetData(string rocket, string field)
+    {
+        if (!_rocketData.ContainsKey(rocket))
+        {
+            Debug.Log($"{rocket} is not a valid key");
+            return ERROR;
+        }
+
+        if (!_rocketData[rocket].ContainsKey(field))
+        {
+            Debug.Log($"{field} is not a valid key for {rocket}");
+            return ERROR;
+        }
+
+        return _rocketData[rocket][field];
+    }
+
+    [ContextMenu("PrintData()")]
+    private void PrintData()
+    {
+        foreach (var rocket in _rocketData.Keys)
+        {
+            Debug.Log($"{rocket}");
+            foreach (var pair in _rocketData[rocket])
+            {
+                Debug.Log($"{pair.Key}:{pair.Value}");
+            }
+        }
+    }
+}
--- HackathonWebGL/Assets/_Features/Rocket/Scripts/JavascriptHook.cs
+++ HackathonWebGL/Assets/_Features/Rocket/Scripts/JavascriptHook.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
@@ -14,16 +15,30 @@ public class JavascriptHook : MonoBehaviour
     public TextMeshProUGUI tons_co2_passenger;
     public TextMeshProUGUI fuelType;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private FirebaseDataHandler firebaseDataHandler;
 
-    // Update is called once per frame
-    void Update()
+    private Dictionary<string, string> _defaultTextValues;
+    
+    private const string NAME = "Name";
+    private const string CO2 = "CO2";
+    private const string FUEL_TYPE = "Fuel Type";
+    private const string KG_CO2 = "KG:CO2";
+    private const string PASSENGERS = "Passengers";
+    private const string TONS_CO2_PASSENGERS = "Tons of CO2 per passenger";
+    private const float TEST_DELAY = 2;
+
+
+    private void Awake()
     {
-       
+        _defaultTextValues = new Dictionary<string, string>
+        {
+            {NAME, name.text},
+            {CO2, co2.text},
+            {KG_CO2, kg_co2.text},
+            {PASSENGERS, passengers.text},
+            {TONS_CO2_PASSENGERS, tons_co2_passenger.text},
+            {FUEL_TYPE, fuelType.text}
+        };
     }
 
     public void LaunchRocket(){
@@ -31,10 +46,69 @@ public class JavascriptHook : MonoBehaviour
     }
 
     public void SelectRocket(string rocketName){
-        //This is where all the fethcing and setting
+        //This is where all the fetching and setting
         //of data needs to happen.
 
         rocketSelector.SelectAndSetShowcaseRocket(rocketName);
         rocketSelector.SelectAndSetLaunchRocket(rocketName);
+        
+        PopulateUI(rocketName);
+    }
+
+    private void PopulateUI(string rocketName)
+    {
+        name.text = $"{_defaultTextValues[NAME]} {rocketName}";
+        
+        var co2Value = firebaseDataHandler.GetData(rocketName, CO2);
+        co2.text = $"{_defaultTextValues[CO2]} {co2Value}";
+        
+        var kgCo2Value = firebaseDataHandler.GetData(rocketName, KG_CO2);
+        kg_co2.text = $"{_defaultTextValues[KG_CO2]} {kgCo2Value}";
+        
+        var passengersValue = firebaseDataHandler.GetData(rocketName, PASSENGERS);
+        passengers.text = $"{_defaultTextValues[PASSENGERS]} {passengersValue}";
+        
+        var tonsPerPassengerValue = firebaseDataHandler.GetData(rocketName, TONS_CO2_PASSENGERS);
+        tons_co2_passenger.text = $"{_defaultTextValues[TONS_CO2_PASSENGERS]} {tonsPerPassengerValue}";
+        
+        var fuelTypeValue = firebaseDataHandler.GetData(rocketName, FUEL_TYPE);
+        fuelType.text = $"{_defaultTextValues[FUEL_TYPE]} {fuelTypeValue}";
+    }
+
+    private void ClearUI()
+    {
+        name.text = $"{_defaultTextValues[NAME]}";
+        co2.text = $"{_defaultTextValues[CO2]}";
+        kg_co2.text = $"{_defaultTextValues[KG_CO2]}";
+        passengers.text = $"{_defaultTextValues[PASSENGERS]}";
+        tons_co2_passenger.text = $"{_defaultTextValues[TONS_CO2_PASSENGERS]}";
+        fuelType.text = $"{_defaultTextValues[FUEL_TYPE]}";
+    }
+    
+    [ContextMenu("Test()")]
+    private void Test()
+    {
+        StartCoroutine(TestCoroutine());
+    }
+
+    private IEnumerator TestCoroutine()
+    {
+        //NAMES MUST MATCH FIREBASE NAMES
+        PopulateUI("Atlas V N22");
+        yield return new WaitForSeconds(TEST_DELAY);
+        PopulateUI("Falcon 9");
+        yield return new WaitForSeconds(TEST_DELAY);
+        PopulateUI("SLS");
+        yield return new WaitForSeconds(TEST_DELAY);
+        PopulateUI("Soyuz FG");
+        yield return new WaitForSeconds(TEST_DELAY);
+        PopulateUI("Space Shuttle");
+        yield return new WaitForSeconds(TEST_DELAY);
+        PopulateUI("Starship + Super Heavy");
+        yield return new WaitForSeconds(TEST_DELAY);
+        PopulateUI("Titan II");
+        yield return new WaitForSeconds(TEST_DELAY);
+        
+        ClearUI();
     }
 }
